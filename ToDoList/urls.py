2f408a822cd9f7912783b3916fa5346a62969fcd
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-03-18 02:33:39 +0530

    Front End completed

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from ToDoList.views import ToDoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='todo/index.html')),
+    path('todo/api/'.ToDoView.as_view())
 ]
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+var app = angular.module('tOdO', []);
+app.controller('tOdOController', function ($scope) {
+    $scope.todoList = [{todoText: 'Finish this app', done: false}];
+    $scope.todoAdd = function () {
+      $scope.todoList.push({todoText: $scope.todoInput,  done: false});
+      $scope.todoInput = '';
+    };
+    $scope.remove = function () {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function (x) {
+            if (!x.done) $scope.todoList.push(x);
+        })
+    }
+});

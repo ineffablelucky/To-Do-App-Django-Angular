93686879ddceb0def180a23c12be57efd63b4e73
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-03-17 23:43:47 +0530

    added todo.js file

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name = 'todo/index.html')),
+    path('', TemplateView.as_view(template_name='todo/index.html')),
 ]

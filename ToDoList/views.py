85d29618b80f53dc25a7213732e848e9abd893d3
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-03-18 20:31:00 +0530

    created a serializer

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from ToDoList.models import ToDoElements
+from ToDoList.serializers import ToDoSerializer
 
 
 class ToDoView(APIView):
 
     def get(self, request):
-        return Response({'test': 'it works'})
+        serializer = ToDoSerializer()
+        return Response(serializer.data)
